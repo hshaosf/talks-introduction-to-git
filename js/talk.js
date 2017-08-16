27c1382859fe6e7ebf4b3bb7e02fec3c6cbd4bca
hui <huiran@gmail.com>
2017-08-15 19:34:18 -0700

    pages for git init/add/commit/push

---
+++ js/talk.js
@@ -0,0 +1,30 @@
+"use strict";
+(function($, _d, _w){
+	var talk = {
+		init : function(){
+			// More info https://github.com/hakimel/reveal.js#configuration
+			Reveal.initialize({
+				controls: true,
+				progress: true,
+				history: true,
+				center: true,
+
+				transition: 'convex', // none/fade/slide/convex/concave/zoom
+
+				// More info https://github.com/hakimel/reveal.js#dependencies
+				dependencies: [
+					{ src: 'lib/js/classList.js', condition: function() { return !document.body.classList; } },
+					{ src: 'plugin/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+					{ src: 'plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+					{ src: 'plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
+					{ src: 'plugin/zoom-js/zoom.js', async: true },
+					{ src: 'plugin/notes/notes.js', async: true }
+				]
+			});
+		}
+	}
+	talk.init();
+	$(_d).ready(function(){
+		console.log('ready');
+	});
+})(jQuery, document, window);
